923e4c4d39e4ea7d9b7e400b30c09580b9d10d3d
ColinSebas <111238679+ColinSebas@users.noreply.github.com>
2022-09-30 14:20:42 -0500

    Add files via upload

--- TexasPrueba.py
+++ TexasPrueba.py
@@ -5,8 +5,7 @@
 import math 
 import random 
 import time
-
-from program import repeticion 
+ 
 
 # Lista de todas las cartas del deck
 
@@ -21,15 +20,10 @@ lista_de_cartas = ["2CR", "3CR","4CR", "5CR", "6CR", "7CR","8CR", "9CR", "10CR"
 
 """
 
-def user_cards(lista):
-    # Función para las cartas del usuario
-    carta_1 = random.choice(lista)
-    return carta_1
-
-def main_cards(lista):
-    # Función para las cinco cartas centrales
-    carta_main = random.choice(lista)
-    return carta_main
+def cards(lista):
+    # Función para repartir cartas al azar
+    carta = random.choice(lista)
+    return carta
 
 def elimina_cartas_de_lista(lista, carta):
     # Función para eliminar cartas de la lista y que no se repitan
@@ -60,23 +54,6 @@ def indices(lista):
         i += 1
 
     return indices 
-    """ ====== Función previan(referencia)
-    
-    if lista[0][0] == '1':
-        indices = ['10']
-    else:
-        indices = [lista[0][0]]
-    i = 1
-    while i < 7:
-        if lista[i][0] == '1':
-            indices.append('10')
-        else:
-            indices.append(lista[i][0])
-        i += 1
-
-    return indices
-
-    """
 
 def palos(lista):
     # Lista de palos de las 7 cartas totales
@@ -119,42 +96,35 @@ def mayor_repeticion(cartas_full):
     return max_val, max_repe, diccionario, dos_val
 
 
-
-def analiza_mejor_juego(cartas_completas):
-    #analizará el mejor juego
-    i = 0
-    pass
-
-
-carta_1 = user_cards(lista_de_cartas)
+carta_1 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_1)
 
-carta_2 = user_cards(lista_de_cartas)
+carta_2 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_2)
 
 cartas_usuario = [carta_1, carta_2]
 
-carta_main_1 = main_cards(lista_de_cartas)
+carta_main_1 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_main_1)
 
-carta_main_2 = main_cards(lista_de_cartas)
+carta_main_2 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_main_2)
 
-carta_main_3 = main_cards(lista_de_cartas)
+carta_main_3 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_main_3)
 
 cartas_main_primeras_tres = [carta_main_1, carta_main_2, carta_main_3]
 
-carta_main_4 = main_cards(lista_de_cartas)
+carta_main_4 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_main_4)
 
-carta_main_5 = main_cards(lista_de_cartas)
+carta_main_5 = cards(lista_de_cartas)
 
 elimina_cartas_de_lista(lista_de_cartas, carta_main_5)
 
@@ -169,17 +139,6 @@ cards_indexes = sorted(indices(cartas_total))
 cards_suits = sorted(palos(cartas_total))
 repeticion_max = mayor_repeticion(cards_indexes)
 
-
-""" ======== Zona de Pruebas de Print ==================
-
-print(cartas_usuario, "\n\n" , cartas_main_primeras_tres, "\n\n", lista_de_cartas)
-
-print(indices_de_cartas(carta_1), indices_de_cartas(carta_2), indices_de_cartas(carta_main_1))
-
-print(cartas_main)
-
- """
-    
 print("Estas son tus cartas: \n", cartas_usuario)
 
 time.sleep(3)
@@ -193,44 +152,39 @@ opcion = int(input("¿Deseas continuar el juego? \n \n 1 = si 0 = no \n"))
 if opcion == 1:
     time.sleep(2)
     print("Siguiente carta: \n", cartas_main_menos1)
-elif opcion == 0:
-    time.sleep(1)
-    print("Te has doblado.")
-else:
-    time.sleep(1)
-    print("La opción introducida es inválida")
-
-time.sleep(2)
-elige_2 = int(input("¿Deseas continuar el juego? \n \n 1 = si 0 = no \n"))
-
-if elige_2 == 1:
     time.sleep(2)
-    print("Última carta: \n", cartas_main)
+    elige_2 = int(input("¿Deseas continuar el juego? \n \n 1 = si 0 = no \n"))
+
+    if elige_2 == 1:
+        time.sleep(2)
+        print("Última carta: \n", cartas_main)
+        time.sleep(2)
+        print(cards_indexes)
+        time.sleep(2)
+        print(repeticion_max)
+        time.sleep(1)
+
+        if len(repeticion_max[3]) == 1:
+            if repeticion_max[1] == 2:
+                print("Tuviste un par de:", repeticion_max[0])
+            elif repeticion_max[1] == 3:
+                print("Tuviste una tercia de:", repeticion_max[0])
+            elif repeticion_max[1] == 4:
+                print("Tuviste un póquer de:", repeticion_max[0])
+        else: 
+            print("Tuviste dos pares de:", repeticion_max[-1][0], "y", repeticion_max[-1][1])
+    elif elige_2 == 0:
+        time.sleep(1)
+        print("Te has doblado.")
+    else:
+        time.sleep(1)
+        print("La opción introducida es inválida")
 elif opcion == 0:
     time.sleep(1)
     print("Te has doblado.")
 else:
     time.sleep(1)
     print("La opción introducida es inválida")
-
-time.sleep(2)
-print(cards_indexes)
-
-time.sleep(2)
-print(repeticion_max)
-
-time.sleep(1)
-
-if len(repeticion_max[3]) == 1:
-    if repeticion_max[1] == 2:
-        print("Tuviste un par de:", repeticion_max[0])
-    elif repeticion_max[1] == 3:
-        print("Tuviste una tercia de:", repeticion_max[0])
-    elif repeticion_max[1] == 4:
-        print("Tuviste un póquer de:", repeticion_max[0])
-else: 
-    print("Tuviste dos pares de:", repeticion_max[-1][0], "y", repeticion_max[-1][1])
-
 
 
 
